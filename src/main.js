312c3979c5b15a8ad65794c5bde3650aa865aa35
zhenyutsai <zhenyutsai@foxmail.com>
2022-06-18 19:00:44 +0800

    添加环境变量和vconsole

--- src/main.js
+++ src/main.js
@@ -4,4 +4,11 @@ import router from './router'
 import store from './store'
 import middleware from './middleware'
 import 'amfe-flexible'
+import VConsole from 'vconsole'
+
+if (process.env.VUE_APP_VCONSOLE === 'true') {
+  // eslint-disable-next-line
+  const vConsole = new VConsole()
+}
+
 createApp(App).use(store).use(router).use(middleware).mount('#app')
