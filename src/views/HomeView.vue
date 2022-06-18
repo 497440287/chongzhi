d5a391bf501e1659c52dd42f65a3a5bfc407879f
zhenyutsai <zhenyutsai@foxmail.com>
2022-06-18 16:41:10 +0800

    集成vant

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,17 +2,28 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <van-button type="primary" size="small">主要按钮</van-button>
+    <div class="demo">
+      demo
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
-
+import { Button } from 'vant'
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    HelloWorld,
+    [Button.name]: Button
   }
 }
 </script>
+
+<style lang="less" scoped>
+.demo{
+  font-size: 48px;
+}
+</style>
