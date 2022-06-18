c9f1c963e12a135caa9befdb121b0841b3538c97
zhenyutsai <zhenyutsai@foxmail.com>
2022-06-18 19:31:00 +0800

    修改样式

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,9 @@
 module.exports = {
   plugins: {
     'postcss-pxtorem': {
-      rootValue: 37.5,
+      rootValue ({ file }) {
+        return file.indexOf('vant') !== -1 ? 37.5 : 75
+      },
       propList: ['*']
     }
   }
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import middleware from './middleware'
+import '@/assets/css/base.css'
 import 'amfe-flexible'
 import VConsole from 'vconsole'
 
