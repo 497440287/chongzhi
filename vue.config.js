988fe1274034efb7ca9e1bf2a06e824abcee972e
zhenyutsai <zhenyutsai@foxmail.com>
2022-06-20 09:21:57 +0800

    修改环境变量

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 const { VantResolver } = require('unplugin-vue-components/resolvers')
 const ComponentsPlugin = require('unplugin-vue-components/webpack')
-
+console.log('当前运行环境', process.argv[2], process.argv[4])
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
