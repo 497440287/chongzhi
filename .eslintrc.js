239001d9613861599e0dbefd83608df6fe847d60
zhenyutsai <zhenyutsai@foxmail.com>
2022-06-22 11:20:54 +0800

    修改校验规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,8 +11,9 @@ module.exports = {
     parser: '@babel/eslint-parser'
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-alert': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': 'off',
     'vue/multi-word-component-names': 'off'
   },
   globals: {
