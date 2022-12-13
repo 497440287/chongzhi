5db679914e128488747d6b47ce2393da0e97ee68
zhenyutsai <zhenyutsai@foxmail.com>
2022-12-13 11:12:09 +0800

    修改鉴权判断方式

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -2,7 +2,12 @@ import router from '@/router'
 import wxAuth from './modules/wxAuth'
 const middleware = () => {
   router.beforeEach((to, from, next) => {
-    wxAuth(to, from, next)
+    // 判断是否是微信环境
+    if (/MicroMessenger/i.test(window.navigator.userAgent)) {
+      wxAuth(to, from, next)
+    } else {
+      next()
+    }
   })
 }
 
--- src/middleware/modules/wxAuth.js
+++ src/middleware/modules/wxAuth.js
@@ -1,14 +1,11 @@
 import store from '@/store'
 const appid = process.env.VUE_APP_WX_APPID
 export default (to, from, next) => {
-  // ios分享链接问题解决
-  if (/MicroMessenger/i.test(window.navigator.userAgent)) {
-    // 判断安卓还是ios环境,记录ios第一个路径值
-    if (window.wxSignLink || /(Android)/i.test(navigator.userAgent)) {
-      window.wxSignLink = location.href
-    }
-    // 在vue文件中直接使用wxSignLink获取签名授权地址
+  // 判断安卓还是ios环境,记录ios第一个路径值
+  if (window.wxSignLink || /(Android)/i.test(navigator.userAgent)) {
+    window.wxSignLink = location.href
   }
+  // 在vue文件中直接使用wxSignLink获取签名授权地址
 
   if (to.meta.wxAuth) {
     console.log('输出当前路径：', location.href)
@@ -25,10 +22,9 @@ export default (to, from, next) => {
         store.dispatch('setUserInfo', { name: '哈哈', role: 'admin', userId: '001' })
         return next()
       }, 1000)
-    } else if (/MicroMessenger/i.test(window.navigator.userAgent)) { // 进入微信回调鉴权
+    } else {
+      // 进入微信回调鉴权
       location.href = wxRedirectUrl(location.href)
-    } else { // 判断是否是微信环境
-      return next()
     }
   } else {
     return next()
